7edc388b92917b03c2a9dfbd78cb173395869fd2
Ivan4489 <ivan.piatygin@hotmail.com>
2023-01-30 20:31:18 +0200

    Task4 is done

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,10 +1,40 @@
-// Пробую. Не відноситься до виконання завдання. Репета 19:45 заняття 13
+// // Пробую. Не відноситься до виконання завдання. Репета 19:45 заняття 13
 
-const btnEl = document.querySelector("button");
+// const btnEl = document.querySelector("button");
 
-console.log(btnEl);
+// console.log(btnEl);
 
-btnEl.addEventListener("click", () => {
-  const counterEl = document.querySelector("#value");
-  console.log(counterEl);
-});
+// btnEl.addEventListener("click", () => {
+//   const counterEl = document.querySelector("#value");
+//   console.log(counterEl);
+// });
+
+let counterValue = 0;
+const valueRef = document.querySelector("#value");
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+
+const onClickDecrementBtn = () => {
+  counterValue -= 1;
+  valueRef.textContent = counterValue;
+};
+
+const onClickIncrementBtn = () => {
+  counterValue += 1;
+  valueRef.textContent = counterValue;
+};
+
+decrementBtn.addEventListener("click", onClickDecrementBtn);
+
+incrementBtn.addEventListener("click", onClickIncrementBtn);
+
+
+// decrementBtn.addEventListener('click', () => {
+//   counterValue -=1;
+//   valueRef.textContent = counterValue;
+// });
+
+// incrementBtn.addEventListener('click', () => {
+//   counterValue +=1;
+//   valueRef.textContent = counterValue;
+// });
