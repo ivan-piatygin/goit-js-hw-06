bdcde3c8ed667e99d7efd4a088e9e37d00599136
Ivan4489 <ivan.piatygin@hotmail.com>
2023-02-05 11:42:57 +0200

    Task9 is done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const textColor = document.querySelector('.color');
+const changeColorBtn = document.querySelector('.change-color');
+const body = document.querySelector('body');
+
+const changeColor = () => {
+  textColor.textContent = getRandomHexColor();
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+changeColorBtn.addEventListener('click', changeColor);
