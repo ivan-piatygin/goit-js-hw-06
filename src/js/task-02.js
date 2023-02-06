40e2b5dff666a7eb8d2087bb1bf1f1ec31cb7069
Ivan4489 <ivan.piatygin@hotmail.com>
2023-02-06 19:35:06 +0200

    Task-02 redone.

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -8,15 +8,24 @@ const ingredients = [
 ];
 
 // 1. Створюється змінна для списку.
-const ingredientsList = document.querySelector("#ingredients");
+const ingredientsList = document.querySelector('#ingredients');
 
-for (const ingredient of ingredients) {
-  // 2. Створює "лішкі"
-  const ingredientEl = document.createElement("li");
-  // 3. Вставляє у "лішкі" текст (елемент заданого масиву)
-  ingredientEl.textContent = ingredient;
-  // 4. Вставка "лішкі" у список
-  ingredientsList.append(ingredientEl);
-}
+const itemsRef = ingredients.map((ingredient) => {
+  const item = document.createElement('li');
+  item.textContent = ingredient;
+  item.classList.add('item');
+  return item;
+});
 
-console.log(ingredientsList);
+ingredientsList.append(...itemsRef);
+
+// for (const ingredient of ingredients) {
+//   // 2. Створює "лішкі"
+//   const ingredientEl = document.createElement("li");
+//   // 3. Вставляє у "лішкі" текст (елемент заданого масиву)
+//   ingredientEl.textContent = ingredient;
+//   // 4. Вставка "лішкі" у список
+//   ingredientsList.append(ingredientEl);
+// }
+
+// console.log(ingredientsList);
